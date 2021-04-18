303061ea7452b0bcd04122669f92f1e41e4e9ec1
unknown <gvaidya4@gmail.com>
2021-04-18 15:53:51 +0530

    code-updated-18Apr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Page6Component } from './page6/page6.component';
+import { Page7Component } from './page7/page7.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page6Component,
+    Page7Component
   ],
   imports: [
     BrowserModule
