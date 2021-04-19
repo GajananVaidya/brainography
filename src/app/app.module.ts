6e9b0a07cd9fd2981cf206c50a5f205704532839
unknown <gvaidya4@gmail.com>
2021-04-19 20:17:17 +0530

    footer-added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { Page6Component } from './page6/page6.component';
 import { Page7Component } from './page7/page7.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Page6Component,
-    Page7Component
+    Page7Component,
+    FooterComponent
   ],
   imports: [
     BrowserModule
