ec239fe63115e131e43e8a2d7e8e44463b328c7a
unknown <gvaidya4@gmail.com>
2021-04-21 16:03:19 +0530

    code-updated-21Apr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { Page6Component } from './page6/page6.component';
 import { Page7Component } from './page7/page7.component';
 import { FooterComponent } from './footer/footer.component';
+import { Page8Component } from './page8/page8.component';
+import { Page9Component } from './page9/page9.component';
+import { Page10Component } from './page10/page10.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Page6Component,
     Page7Component,
-    FooterComponent
+    FooterComponent,
+    Page8Component,
+    Page9Component,
+    Page10Component
   ],
   imports: [
     BrowserModule
